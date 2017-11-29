da9f18ffc50ed6800c1c8242428e9cd5d0712306
yang <903926899@qq.com>
2017-11-29 09:21:52 +0800

    vue吃了么

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+// import '../node_modules/bootstrap/dist/js/bootstrap.js'
+// import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './common/stylus/index.styl'
+
+Vue.use(Vuex)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App },
+})
---
+++ src/vuex/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  // 定义状态
+  state: {
+    
+  },
+  actions: {
+
+  },
+  mutations: {
+
+  },
+  getters: {
+
+  },modules: {
+    
+  }
+})
+
+export default store
